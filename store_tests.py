24c7ba2b4a9abc4ffac3d15811d77874ef7c1f5e
Goujer <legojr33@gmail.com>
2020-04-22 17:32:39 -0500

    Tests now pass
    There are missing items.

--- store_tests.py
+++ store_tests.py
@@ -14,7 +14,27 @@ class StoreTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(StoreTest, self).__init__(*args, **kwargs)
         self.sm = StoreManager()
-        self.user = {"facebook_connect": False, "money": 0, "sex": "male", "create_time": 1413019326, "likes": [], "id": 1, "description": None, "uuid": ["test_uuid"], "stamina": 100, "kanojos": [1, 368], "email": None, "twitter_connect": False, "generate_count": 0, "profile_image_url": "http://www.deviantsart.com/2oo69ib.jpg", "birthday": 1413025200, "enemies": [], "password": None, "friends": [231, 31, 149, 333, 335, 336, 337, 339, 30, 220, 361], "tickets": 20, "name": "everyone", "language": "en", "level": 1, "scan_count": 0, "has_items": [{"has_item_id": 51, "store_item_id": 110}]}
+        self.user = {"facebook_connect": False,
+			"money": 0,
+			"sex": "male",
+			"create_time": 1413019326, "likes": [],
+			"id": 1, "description": None,
+			"uuid": ["test_uuid"],
+			"stamina": 100,
+			"kanojos": [1, 368],
+			"email": None,
+			"twitter_connect": False,
+			"generate_count": 0,
+			"profile_image_url": "http://www.deviantsart.com/2oo69ib.jpg",
+			"birthday": 1413025200, "enemies": [],
+			"password": None,
+			"friends": [231, 31, 149, 333, 335, 336, 337, 339, 30, 220, 361],
+			"tickets": 20,
+			"name": "everyone",
+			"language": "en",
+			"level": 1,
+			"scan_count": 0,
+			"has_items": [{"has_item_id": 51, "store_item_id": 110}]}
 
     def prepare_categories(self, ctgrs, user_level=None):
         item_categories = []
@@ -22,7 +42,7 @@ class StoreTest(unittest.TestCase):
         while len(_ctgrs):
             c = _ctgrs[0]
             group = [c, ]
-            # serach category group
+            # search category group
             if 'group_title' in c:
                 group = [x for x in _ctgrs if x.get('group_title') == c.get('group_title')]
             val = {
